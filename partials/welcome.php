0aa3e9baec2eadb4fd082d42cf27f48810b02c76
cesar <caesarnetyet@gmail.com>
2022-07-28 15:49:30 -0500

    sessiones

---
+++ partials/welcome.php
@@ -0,0 +1,16 @@
+<header class="vh-75 container d-none d-md-block">
+    <div class="row align-items-center">
+    <figure class="col-md-6 ">
+        <img class="img-fluid" src="assets/meteor-rain.gif" alt="">
+    </figure>
+    <section class="col-md-4 offset-md-1">
+        <h1 class="">Bienvenido a la tienda de juegos</h1>
+        <p class="">
+            Aquí podrás encontrar los juegos más populares de todos los tiempos.
+        </p>
+        <div class="">
+            <a href="juegos.php" class="btn btn-success">Ver juegos</a>
+        </div>
+    </section>
+    </div>
+</header>
