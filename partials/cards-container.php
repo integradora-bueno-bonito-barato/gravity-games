83275038e1cdb7747f115a3ca3d3c2030ca434c8
cesar <caesarnetyet@gmail.com>
2022-07-27 23:07:02 -0500

    agregar tarjetas extra

--- partials/cards-container.php
+++ partials/cards-container.php
@@ -1,9 +1,13 @@
 <div class="container">
     <div class="row">
-        <div class="d-none d-md-block col-md-4">Categorias</div>
+        <div class="mt-5 d-none d-md-block col-md-4 bg-light rounded-3">
+            <h2>Filtros</h2> 
+        </div>
         <div class="col-12 col-md-8">
             <h1 class="text-center text-md-start">Recomendaciones</h1>
-            <?php include('partials/cards.php');?>
+            <div class="contenedor-tarjeta d-md-flex gap-md-3 flex-wrap ">
+                <?php include('partials/cards.php');?>
+            </div>
         </div>
     </div>
 </div>
--- partials/cards.php
+++ partials/cards.php
@@ -1,5 +1,5 @@
-<div class="tarjeta mx-auto mx-md-0 bg-dark d-flex align-items-md-center justify-items-center flex-md-column p-3  text-light rounded-3" style="--bs-bg-opacity: .9;">
-    <img class="d-block w-100" src="assets/img-juegos/elden-ring.jpeg" alt="">
+<div class="tarjeta my-2 mx-auto mx-md-0 bg-dark d-flex align-items-md-center justify-items-center flex-md-column p-3  text-light rounded-3" style="--bs-bg-opacity: .9;">
+    <img class="d-block" src="assets/img-juegos/elden-ring.jpeg" alt="">
     <div class="tarjeta-contenido w-100 d-flex flex-column align-items-center align-items-md-start ms-2 ms-0-md">
         <div class="d-md-flex w-100 justify-content-md-between align-items-md-center">
             <h3 class="fs-5">Elden Ring</h3>
@@ -7,6 +7,44 @@
         </div>
         <p>Plataforma:</p>
         <p>Steam</p>
-        <a href="elden-ring.php" class="btn btn-primary mt-md-2 d-block mt-auto">Añadir al carrito</a>
+        <a href="elden-ring.php" class="btn btn-success mt-md-2 d-block mt-auto">Añadir al carrito</a>
+    </div>
+</div>
+
+<div class="tarjeta my-2  mx-auto mx-md-0 bg-dark d-flex align-items-md-center justify-items-center flex-md-column p-3  text-light rounded-3" style="--bs-bg-opacity: .9;">
+    <img class="d-block" src="assets/img-juegos/outlast.jpg" alt="">
+    <div class="tarjeta-contenido w-100 d-flex flex-column align-items-center align-items-md-start ms-2 ms-0-md">
+        <div class="d-md-flex w-100 justify-content-md-between align-items-md-center">
+            <h3 class="fs-5">Outlast</h3>
+            <p>$399.99</p>
+        </div>
+        <p>Plataforma:</p>
+        <p>Steam</p>
+        <a href="elden-ring.php" class="btn btn-success mt-md-2 d-block mt-auto">Añadir al carrito</a>
+    </div>
+</div>
+<div class="tarjeta my-2 mx-auto mx-md-0 bg-dark d-flex align-items-md-center justify-items-center flex-md-column p-3  text-light rounded-3" style="--bs-bg-opacity: .9;">
+    <img class="d-block" src="assets/img-juegos/silent-hill.jpg" alt="">
+    <div class="tarjeta-contenido w-100 d-flex flex-column align-items-center align-items-md-start ms-2 ms-0-md">
+        <div class="d-flex flex-column  justify-content-md-between align-items-md-center">
+            <h3 class="fs-5">Silent Hill</h3>
+            <p>$444.99</p>
+        </div>
+        <p>Plataforma:</p>
+        <p>Playstation 4</p>
+        <a href="elden-ring.php" class="btn btn-success mt-md-2 d-block mt-auto">Añadir al carrito</a>
+    </div>
+</div>
+
+<div class="tarjeta my-2 mx-auto mx-md-0 bg-dark d-flex align-items-md-center justify-items-center flex-md-column p-3  text-light rounded-3" style="--bs-bg-opacity: .9;">
+    <img class="d-block" src="assets/img-juegos/granny.png" alt="">
+    <div class="tarjeta-contenido w-100 d-flex flex-column align-items-center align-items-md-start ms-2 ms-0-md">
+        <div class="d-md-flex w-100 justify-content-md-between align-items-md-center">
+            <h3 class="fs-5">Granny</h3>
+            <p>$124.99</p>
+        </div>
+        <p>Plataforma:</p>
+        <p>Steam</p>
+        <a href="elden-ring.php" class="btn btn-success mt-md-2 d-block mt-auto">Añadir al carrito</a>
     </div>
 </div>
