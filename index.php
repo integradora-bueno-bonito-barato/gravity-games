ce40d99c72e4355dfe9c140a0906614d3e15d84f
guillermo <guillermojr2512@gmail.com>
2022-07-30 16:48:12 -0500

    Agregue mas texto

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ y poder incluir codigo en otras partes de la pagina. -->
 
 <h1 class="p-5 text-center">Hello world</h1>
 
-
+este es un c
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis harum sed distinctio deserunt veniam voluptates debitis odit ut, illo quas eum aliquid nihil eveniet tenetur necessitatibus itaque magnam labore earum?
 
 <!-- body -->
 
