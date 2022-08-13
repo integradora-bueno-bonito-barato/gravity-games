c27adb7a0534e751de61be672bd785f9e321443f
guillermo <guillermojr2512@gmail.com>
2022-08-12 20:40:23 -0500

    hice que funcione la barra de busqueda

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!-- los includes son para no saturar el codigo de la pagina, 
 y poder incluir codigo en otras partes de la pagina. -->
-
+<?php include 'conexion.php';?>
 <?php 
-  session_start();
+session_start();
 include('./partials/header.php');?>
 <?php include('./partials/welcome.php');?>
 <?php include('./partials/carousel.php');?>
