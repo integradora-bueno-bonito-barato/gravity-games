c8210ee0066d70b69a095e689ec9acea1bb6a5c5
cesar <caesarnetyet@gmail.com>
2022-08-16 00:54:31 -0500

    bugfix temporal

--- partials/nav.php
+++ partials/nav.php
@@ -19,6 +19,11 @@
                       echo '<div class="alert alert-success text-center">'.$_SESSION['compra'].'</div>';
                       unset($_SESSION['compra']);
                   } ;?>
+                   <?php
+                    if(isset($_SESSION['agregado'])) {
+                      echo '<div class="alert alert-success text-center">'.$_SESSION['agregado'].'</div>';
+                      unset($_SESSION['agregado']);
+                  } ;?>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
