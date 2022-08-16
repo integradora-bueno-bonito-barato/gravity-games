80f6a1521f0ee90301035b4021e75db0189b52a3
cesar <caesarnetyet@gmail.com>
2022-08-16 16:54:59 -0500

    modificaciones

--- index.php
+++ index.php
@@ -3,17 +3,17 @@ y poder incluir codigo en otras partes de la pagina. -->
 <?php include 'conexion.php';?>
 <?php 
 session_start();
-include('./partials/header.php');?>
-<?php include('./partials/welcome.php');?>
-<?php include('./partials/carousel.php');?>
+include('partials/header.php');?>
+<?php include('partials/welcome.php');?>
+<?php include('partials/carousel.php');?>
 
-<?php include('./partials/cards-container.php');?>
-
-<!-- body -->
-
+<?php include('partials/cards-container.php');?>
 
 <!-- body -->
 
 
+<!-- body -->
 
-<?php include('./partials/footer.php');?>
+
+
+<?php include('partials/footer.php');?>
